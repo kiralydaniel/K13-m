47ad3be10943ff6515c28c5faf0f1100d1d5e37d
kiralydaniel <kiralydani93@gmail.com>
2023-09-21 20:03:05 +0200

    Create Footer

---
+++ components/Footer.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="bg-white shadow m-4 dark:bg-gray-800 mx-auto max-w-full">
+        <div class="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
+            <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <NuxtLink to="/" class="hover:underline">K13 MESTER</NuxtLink>
+            </span>
+            <ul class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-700 md:dark:bg-gray-900 dark:border-gray-700">
+                <li>
+                <NuxtLink to="/" class="block py-2 pl-3 pr-4 text-gray-400 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-sky-400 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Kezdőlap</NuxtLink>
+                </li>
+                <li>
+                <NuxtLink to="kreak2" class="block py-2 pl-3 pr-4 text-gray-400 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-sky-400 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Krea K2</NuxtLink>
+                </li>
+                <li>
+                <NuxtLink to="contact" class="block py-2 pl-3 pr-4 text-gray-400 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-sky-400 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Kapcsolat</NuxtLink>
+                </li>
+            </ul>
+        </div>
+    </div>
+</template>
