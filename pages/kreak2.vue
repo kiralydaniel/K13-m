fba9063eae241dcb73883fcdc31ad8affc78bf1a
kiralydaniel <kiralydani93@gmail.com>
2023-09-21 23:42:09 +0200

    Update kreak2 cards

--- pages/kreak2.vue
+++ pages/kreak2.vue
@@ -13,61 +13,68 @@
       <rect width="100%" height="100%" stroke-width="0" fill="url(#e813992c-7d03-4cc4-a2bd-151760b470a0)" />
     </svg>
   </div>
-    <div class="mx-auto max-w-7xl px-6 lg:px-8">
-      <div class="mx-auto max-w-2xl lg:text-center">
-        <p class="text-2xl font-bold tracking-tight text-sky-400 sm:text-4xl">A KREA K2+ korszerűsítés bemutatása</p>
-        <p class="mt-6 text-2xl font-bold tracking-tight text-gray-900 sm:text-4xl">Így készül a CONSTRUMA díjas Krea K2+ rendszer:</p>
-      </div>
-      <div class="mx-auto mt-16 max-w-2xl sm:mt-20 lg:mt-24 lg:max-w-4xl">
-        <dl class="grid max-w-xl grid-cols-1 gap-x-8 gap-y-10 lg:max-w-none lg:grid-cols-2 lg:gap-y-16">        
-            
-          <div class="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
-              <img class="object-cover aspect-[6/10] w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg" src="/krea01.jpg" alt="">
-              <div class="flex flex-col justify-between p-4 leading-normal">
-                  <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Előkészítés</h5>
-                  <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Előkészítjük a gyűjtőkéményt a mérésre, valamint a KREA K2+ fogadására, a meglévő készülékek ideiglenes lecsatlakoztatásával.</p>
-              </div>
-          </div>
-          <div class="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">              
-              <img class="object-cover aspect-[6/10] w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg" src="/krea02.jpg" alt="">
-              <div class="flex flex-col justify-between p-4 leading-normal">
-                  <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Mérés</h5>
-                  <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">A kémény adatait részletesen megmérjük, a meglévő bekötések pontos helyzetének meghatározásával.</p>
-              </div>
-          </div>
-          <div class="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
-              <img class="object-cover aspect-[6/10] w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg" src="/krea03.jpg" alt="">
-              <div class="flex flex-col justify-between p-4 leading-normal">
-                  <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Összeállítás</h5>
-                  <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Az adatok alapján, a rendszerelemeket összeállítjuk, majd a kitorkolláson bevezetve, a tetőről béleljük teljes hosszában a közös kéményt.</p>
-              </div>
-          </div>
-          <div class="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
-              <img class="object-cover aspect-[6/10] w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg" src="/krea04.jpg" alt="">
-              <div class="flex flex-col justify-between p-4 leading-normal">
-                  <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Beépítés</h5>
-                  <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">A kazáncsatlakoztatásoknál beépítjük a speciális füstgázcsappantyúkat, valamint kialakítjuk az alsó kondenzelvezetési pontot is.</p>
-              </div>
-          </div>
-          <div class="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
-              <img class="object-cover aspect-[6/10] w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg" src="/krea05.jpg" alt="">
-              <div class="flex flex-col justify-between p-4 leading-normal">
-                  <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Ellenőrzés</h5>
-                  <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Mérőműszerrel ellenőrizzük a megfelelő nyomásállóságot.</p>
-              </div>
-          </div>
-          <div class="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
-              <img class="object-cover aspect-[6/10] w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg" src="/krea06.jpg" alt="">
-              <div class="flex flex-col justify-between p-4 leading-normal">
-                  <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Visszacsatlakoztatás</h5>
-                  <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">A membrános takarólemezek felhelyezését követően a kazánokat visszacsatlakoztatjuk.</p>
-              </div>
-          </div>
-
-        </dl>
+  <div class="mx-auto max-w-7xl px-6 lg:px-8">
+    <div class="mx-auto max-w-2xl lg:text-center">
+      <p class="text-2xl font-bold tracking-tight text-sky-400 sm:text-4xl">A KREA K2+ korszerűsítés bemutatása</p>
+      <p class="mt-6 text-2xl font-bold tracking-tight text-gray-900 sm:text-4xl">Így készül a CONSTRUMA díjas Krea K2+ rendszer:</p>
+    </div>
+    <div class="mx-auto mt-16 max-w-2xl sm:mt-20 lg:mt-24 lg:max-w-4xl">
+      <dl class="grid max-w-xl grid-cols-1 gap-x-8 gap-y-10 lg:max-w-none lg:grid-cols-2 lg:gap-y-16">        
+          
+        <div class="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+            <img class="object-cover aspect-[6/10] w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg" src="/krea01.jpg" alt="">
+            <div class="flex flex-col justify-between p-4 leading-normal">
+                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Előkészítés</h5>
+                <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Előkészítjük a gyűjtőkéményt a mérésre, valamint a KREA K2+ fogadására, a meglévő készülékek ideiglenes lecsatlakoztatásával.</p>
+            </div>
+        </div>
+        <div class="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">              
+            <img class="object-cover aspect-[6/10] w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg" src="/krea02.jpg" alt="">
+            <div class="flex flex-col justify-between p-4 leading-normal">
+                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Mérés</h5>
+                <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">A kémény adatait részletesen megmérjük, a meglévő bekötések pontos helyzetének meghatározásával.</p>
+            </div>
+        </div>
+        <div class="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+            <img class="object-cover aspect-[6/10] w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg" src="/krea03.jpg" alt="">
+            <div class="flex flex-col justify-between p-4 leading-normal">
+                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Összeállítás</h5>
+                <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Az adatok alapján, a rendszerelemeket összeállítjuk, majd a kitorkolláson bevezetve, a tetőről béleljük teljes hosszában a közös kéményt.</p>
+            </div>
+        </div>
+        <div class="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+            <img class="object-cover aspect-[6/10] w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg" src="/krea04.jpg" alt="">
+            <div class="flex flex-col justify-between p-4 leading-normal">
+                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Beépítés</h5>
+                <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">A kazáncsatlakoztatásoknál beépítjük a speciális füstgázcsappantyúkat, valamint kialakítjuk az alsó kondenzelvezetési pontot is.</p>
+            </div>
+        </div>
+        <div class="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+            <img class="object-cover aspect-[6/10] w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg" src="/krea05.jpg" alt="">
+            <div class="flex flex-col justify-between p-4 leading-normal">
+                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Ellenőrzés</h5>
+                <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Mérőműszerrel ellenőrizzük a megfelelő nyomásállóságot.</p>
+            </div>
+        </div>
+        <div class="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+            <img class="object-cover aspect-[6/10] w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg" src="/krea06.jpg" alt="">
+            <div class="flex flex-col justify-between p-4 leading-normal">
+                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Visszacsatlakoztatás</h5>
+                <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">A membrános takarólemezek felhelyezését követően a kazánokat visszacsatlakoztatjuk.</p>
+            </div>
+        </div>
+      </dl>
       </div>
     </div>
+    <div class="flex flex-col items-center pt-28">
+      <a href="#" class="block max-w-xl p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+        <p class="text-center text-2xl font-bold tracking-tight text-sky-400 sm:text-4xl">Az eredmény</p>
+        <p class="pt-10 text-xl">Minden szempontból korszerű túlnyomásos gyűjtőkémény, amelyen az átalakítást követően bármilyen arányban cserélhetőek kondenzációs gázkazánokra az esetleg elromlott régi készülékek.<br>A kéményre kötött többi, még használatban lévő turbós kazánt nem szükséges egyidejűleg lecserélni!</p>
+      </a>
+    </div>
+
   </div>
+
 </template>
 
   
