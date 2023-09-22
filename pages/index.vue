d03ad5da56764f08f02c6c098b49735fd74ed484
kiralydaniel <kiralydani93@gmail.com>
2023-09-22 16:50:21 +0200

    Create new index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,136 +1,18 @@
 <template>
-<div class="relative isolate overflow-hidden bg-white px-6 py-24 sm:py-32 lg:overflow-visible lg:px-0">
-  <div class="absolute inset-0 -z-10 overflow-hidden">
-    <svg class="absolute left-[max(50%,25rem)] top-0 h-[64rem] w-[128rem] -translate-x-1/2 stroke-gray-200 [mask-image:radial-gradient(64rem_64rem_at_top,white,transparent)]" aria-hidden="true">
-      <defs>
-        <pattern id="e813992c-7d03-4cc4-a2bd-151760b470a0" width="200" height="200" x="50%" y="-1" patternUnits="userSpaceOnUse">
-          <path d="M100 200V.5M.5 .5H200" fill="none" />
-        </pattern>
-      </defs>
-        <svg x="50%" y="-1" class="overflow-visible fill-gray-50">
-          <path d="M-100.5 0h201v201h-201Z M699.5 0h201v201h-201Z M499.5 400h201v201h-201Z M-300.5 600h201v201h-201Z" stroke-width="0" />
-        </svg>
-      <rect width="100%" height="100%" stroke-width="0" fill="url(#e813992c-7d03-4cc4-a2bd-151760b470a0)" />
-    </svg>
-  </div>
-  <div class="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 lg:mx-0 lg:max-w-none lg:grid-cols-2 lg:items-start lg:gap-y-10 rounded-lg">
-    <div class="lg:col-span-2 lg:col-start-1 lg:row-start-1 lg:mx-auto lg:grid lg:w-full lg:max-w-7xl lg:grid-cols-2 lg:gap-x-8 lg:px-8">
-      <div class="lg:pr-4">
-        <div class="lg:max-w-lg">
-          <h1 class="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Kazáncsere a gyűjtőkéményen? Nem probléma!</h1>
-          <p class="mt-6 text-xl leading-8 text-gray-700">Közös kémény, közös érdek a társasházi gyűjtőkéményen! </p>
-        </div>
-      </div>
+  <Carousel></Carousel>
+  <div class="relative isolate overflow-hidden bg-white px-6 sm:py-32 lg:overflow-visible lg:px-0">
+      <div class="absolute inset-0 -z-10 overflow-hidden">
+      <svg class="absolute left-[max(50%,25rem)] top-0 h-[64rem] w-[128rem] -translate-x-1/2 stroke-gray-200 [mask-image:radial-gradient(64rem_64rem_at_top,white,transparent)]" aria-hidden="true">
+        <defs>
+          <pattern id="e813992c-7d03-4cc4-a2bd-151760b470a0" width="200" height="200" x="50%" y="-1" patternUnits="userSpaceOnUse">
+            <path d="M100 200V.5M.5 .5H200" fill="none" />
+          </pattern>
+        </defs>
+          <svg x="50%" y="-1" class="overflow-visible fill-gray-50">
+            <path d="M-100.5 0h201v201h-201Z M699.5 0h201v201h-201Z M499.5 400h201v201h-201Z M-300.5 600h201v201h-201Z" stroke-width="0" />
+          </svg>
+        <rect width="100%" height="100%" stroke-width="0" fill="url(#e813992c-7d03-4cc4-a2bd-151760b470a0)" />
+      </svg>
     </div>
-    <div class="-ml-12 -mt-12 p-12 lg:sticky lg:top-4 lg:col-start-2 lg:row-span-2 lg:row-start-1 lg:overflow-hidden">
-      <img class="w-[48rem] max-w-12 rounded-xl bg-gray-900 shadow-xl ring-1 ring-gray-400/10 sm:w-[57rem]" src="/K2Modell.png" alt="">
-      <p class="text-center">Gyűjtőkémény korszerűsítés KREA K2+</p>
     </div>
-    <div class="lg:col-span-2 lg:col-start-1 lg:row-start-2 lg:mx-auto lg:grid lg:w-full lg:max-w-7xl lg:grid-cols-2 lg:gap-x-8 lg:px-8">
-      <div class="lg:pr-4">
-        <div class="max-w-xl text-base leading-7 text-gray-700 lg:max-w-lg">
-          <p><strong>Amikor le kell cserélni a turbós gázkazánt a társasházi közös kéményen, akkor figyelembe kell venni azt, hogy 2018-tól,</strong> az úgynevezett ErP rendelet alapján, a meglévő kazánokat cserélni, 
-            csak kondenzációs gázkazánokra jellemző hatásfokú készülékekre lehet.</p>
-          <ul role="list" class="mt-8 space-y-8 text-gray-600">
-            <li class="flex gap-x-3">
-              <svg class="w-6 h-6 text-sky-400 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-                <path d="m19.707 9.293-2-2-7-7a1 1 0 0 0-1.414 0l-7 7-2 2a1 1 0 0 0 1.414 1.414L2 10.414V18a2 2 0 0 0 2 2h3a1 1 0 0 0 1-1v-4a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v4a1 1 0 0 0 1 1h3a2 2 0 0 0 2-2v-7.586l.293.293a1 1 0 0 0 1.414-1.414Z"/>
-              </svg>
-              <span><strong class="font-semibold text-gray-900">Társasházi gyűjtőkémények</strong></span>
-            </li>
-            <li class="flex gap-x-3">
-              <svg class="w-6 h-6 text-sky-400 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 16">
-                <path d="M18 0H6a2 2 0 0 0-2 2h10a4 4 0 0 1 4 4v6a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2Z"/>
-                <path d="M14 16H2a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2Z"/>
-                <path d="M8 13a3 3 0 1 1 0-6 3 3 0 0 1 0 6Zm0-4a1 1 0 1 0 0 2 1 1 0 0 0 0-2Z"/>
-              </svg>
-              <span><strong class="font-semibold text-gray-900">Cserélendő turbós kazán</strong></span>
-            </li>
-            <li class="flex gap-x-3">
-              <svg class="w-6 h-6 text-sky-400 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 15 20">
-                <path d="M9.092 18h-4a1 1 0 0 0 0 2h4a1 1 0 0 0 0-2Zm-2-18a7.009 7.009 0 0 0-7 7 7.8 7.8 0 0 0 2.219 5.123c.956 1.195 1.781 2.228 1.781 3.877a1 1 0 0 0 1 1h4a1 1 0 0 0 1-1c0-1.7.822-2.7 1.774-3.868A7.63 7.63 0 0 0 14.092 7a7.009 7.009 0 0 0-7-7Zm0 5a2 2 0 0 0-2 2 1 1 0 0 1-2 0 4 4 0 0 1 4-4 1 1 0 0 1 0 2Z"/>
-               </svg>
-              <span><strong class="font-semibold text-gray-900">Mi a megoldás?</strong></span>
-            </li>
-            <li class="flex gap-x-3">
-              <svg class="w-6 h-6 text-sky-400 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 16 12">
-                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5.917 5.724 10.5 15 1.5"/>
-              </svg>
-              <span><strong class="font-semibold text-gray-900">Gyűjtőkémény korszerűsítés</strong></span>
-            </li>
-          </ul>
-          <p class="mt-8">A meglévő társasházi közös kéményeket át kell alakítani olyan túlnyomásos, kondenzációs gyűjtőkéménnyé, amely mind a magas hőmérsékletű (turbós), mind az alacsony hőmérsékletű, 
-            sok kondenzátumot tartalmazó (kondenzációs kazán) égésterméknek is ellenáll, ezáltal alkalmas az újonnan felszerelt kondenzációs kazánok füstgáz elvezetésére is, akár vegyesen csatlakoztatva.</p>
-          <p class="mt-6">A Krea K2+ nemesacél béléscső és innovatív csatlakozó rendszer, valamint a magas hőállóságú védő füstgázcsappantyúk alkalmazásával már lehetséges, hogy az átalakított kéményen 
-            a turbós és a kondenzációs kazánok akár vegyesen is üzemelhessenek. <strong>A rendszer műszaki engedéllyel rendelkezik egy szinten több készülék csatlakoztatására is!</strong></p>
-        </div>
-      </div>
-    </div>
-  </div>
-</div>
-<div class="overflow-hidden bg-white flex items-center justify-center">
-    <div class="mx-auto max-w-7xl px-6 lg:px-8">
-      <h1 class="mb-8 text-2xl font-semibold text-gray-900 dark:text-white">Biztonság és korszerűség! Az átalakítás eredményeként:</h1>
-      <ul class="space-y-3 text-gray-900 list-disc list-inside dark:text-gray-400">
-          <li>
-            A saválló nemesacél P1 nyomásosztályú béléscső garantálja a kialakított rendszer tartósságát, a nyomás és kondenzvíz állóságát.
-          </li>
-          <li>
-            A bekötőnyílásokba épített speciális füstgázcsappantyú megvédi a túlnyomástól és a kondenzvíztől az éppen nem működő készülékeket.
-          </li>
-          <li>
-            A saválló acél kondenzvíz elvezető rendszere védelmet nyújt a kémény átnedvesedése ellen is.
-          </li>
-          <li>
-            A gyűjtőkémény átalakítás előzetes felmérés alapján, nagyobb bontások nélkül, egy nap alatt elvégezhető.
-          </li>
-      </ul>
-  </div>
-</div>
-<div class="relative isolate overflow-hidden bg-white px-6 py-24 sm:py-32 lg:overflow-visible lg:px-0">
-  <div class="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 lg:mx-0 lg:max-w-none lg:grid-cols-2 lg:items-start lg:gap-y-10">
-    <div class="lg:col-span-2 lg:col-start-1 lg:row-start-1 lg:mx-auto lg:grid lg:w-full lg:max-w-7xl lg:grid-cols-2 lg:gap-x-8 lg:px-8">
-      <div class="lg:pr-4">
-        <div class="lg:max-w-lg">
-          <h1 class="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Miért érdemes a CONSTRUMA díjas KREA K2+ rendszert választani?</h1>
-          <p class="mt-6 text-xl leading-8 text-gray-700">Megmarad a gyűjtőkémény jelleg, de már nem szükséges a közös kéményre csatlakozó többi, de problémamentesen működő kazánokat is lecserélni!</p>
-        </div>
-      </div>
-    </div>
-    <div class="-ml-12 -mt-12 p-12 lg:sticky lg:top-4 lg:col-start-2 lg:row-span-2 lg:row-start-1 lg:overflow-hidden">
-      <img class="w-[48rem] max-w-12 rounded-xl shadow-xl ring-1 ring-gray-400/10 sm:w-[57rem]" src="/construma.png" alt="">
-    </div>
-    <div class="lg:col-span-2 lg:col-start-1 lg:row-start-2 lg:mx-auto lg:grid lg:w-full lg:max-w-7xl lg:grid-cols-2 lg:gap-x-8 lg:px-8">
-      <div class="lg:pr-4">
-        <div class="max-w-xl text-base leading-7 text-gray-700 lg:max-w-lg">
-          <p>Ha elromlik az Ön meglévő turbós kazánja, a többi lakótól függetlenül, további kéményátalakítások nélkül, gyorsan lecserélheti a készülékét. Ez, egy téli időszakban nagyon fontos szempont lehet, hiszen nem mindegy, hogy napokig, vagy esetleg hónapokig marad fűtés/melegvíz nélkül.</p>
-          <ul role="list" class="list-disc mt-8 space-y-2 px-6 text-gray-600">
-            <li>
-              <span>Rombolás nélkül, 1 nap alatt beépíthető</span>
-            </li>
-            <li>
-              <span><strong>Akár több készülék is csatlakoztatható szintenként!</strong></span>
-            </li>
-            <li>
-              <span>ÉMI ALKALMAZÁSTECHNOLÓGIAI BIZONYÍTVÁNY a rendszerre</span>
-            </li>
-            <li>
-              <span>Nincsenek elektromos alkatrészek, vezetékek, nincs zajt okozó ventilátor</span>
-            </li>
-            <li>
-              <span>Kedvező beépítési árak, további fenntartási költségek nélkül</span>
-            </li>
-            <li>
-              <span>Tartós, kiváló minőségű nemesacél rendszerelemek CE tanúsítvánnyal</span>
-            </li>
-            <li>
-              <span>Garancia az átalakított kéményekre</span>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </div>
-  </div>
-</div>
-
 </template>
