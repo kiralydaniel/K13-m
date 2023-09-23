e8d41580b613527ecbadcd06bfd9c4fdcf395621
kiralydaniel <kiralydani93@gmail.com>
2023-09-23 18:54:22 +0200

    Update links in navbar and footer

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -16,10 +16,10 @@
                 <NuxtLink to="/" @click="closeMobileMenu" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-sky-400 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Kezdőlap</NuxtLink>
                 </li>
                 <li>
-                <NuxtLink to="upgrade" @click="closeMobileMenu" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-sky-400 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Krea K2+</NuxtLink>
+                <NuxtLink to="kreak2" @click="closeMobileMenu" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-sky-400 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Krea K2+</NuxtLink>
                 </li>
                 <li>
-                <NuxtLink to="kreak2" @click="closeMobileMenu" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-sky-400 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Korszerűsítés lépései</NuxtLink>
+                <NuxtLink to="upgrade" @click="closeMobileMenu" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-sky-400 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Korszerűsítés lépései</NuxtLink>
                 </li>
                 <li>
                 <NuxtLink to="contact" @click="closeMobileMenu" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-sky-400 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Kapcsolat</NuxtLink>
